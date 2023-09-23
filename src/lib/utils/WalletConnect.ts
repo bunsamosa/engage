9b85e988afc47aa6d9aca482f58355af31e23cc3
madhukar01 <mholla8@gmail.com>
2023-09-23 13:52:21 -0400

    feat(wallet): walletconnect integration

---
+++ src/lib/utils/WalletConnect.ts
@@ -0,0 +1,41 @@
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi';
+import { mainnet, arbitrum } from '@wagmi/core/chains';
+import { writable } from 'svelte/store';
+import { disconnect, watchAccount } from '@wagmi/core';
+
+
+// wallet connect setup and modal
+const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
+const chains = [mainnet, arbitrum];
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' });
+const modal = createWeb3Modal({ wagmiConfig, projectId, chains });
+
+// data stores
+const address = writable('');
+const connected = writable(false);
+
+// open connection modal
+async function login() {
+    await modal.open();
+};
+
+// watch for changes in account
+watchAccount(async (account) => {
+    if(account.isConnected) {
+        connected.set(true);
+        address.set(account.address);
+        console.log('User is connected');
+    } else {
+        connected.set(false);
+        address.set('');
+        console.log('User is not connected');
+    }
+});
+
+// logout the user
+async function logout() {
+    await disconnect();
+};
+
+export { address, connected, logout, login };
