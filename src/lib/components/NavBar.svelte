dd0c7e44232fdc6c62c70587e777dcf7b38f226d
madhukar01 <mholla8@gmail.com>
2023-09-23 13:51:45 -0400

    feat(app): navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import {AppBar } from '@skeletonlabs/skeleton';
+    import {connected, login, logout} from '$lib/utils/WalletConnect';
+</script>
+
+<AppBar background="">
+    <svelte:fragment slot="lead">
+        <a href="/" class="text-2xl font-semibold">Engage</a>
+    </svelte:fragment>
+    <svelte:fragment slot="trail">
+        {#if $connected}
+        <button class="px-1" on:click={logout}> Logout </button>
+        {:else}
+        <button class="px-1" on:click={login}> Login </button>
+        {/if}
+    </svelte:fragment>
+</AppBar>
