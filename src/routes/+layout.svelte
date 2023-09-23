fdf77e53dced67baa3b975fed85c5e467e1122c2
madhukar01 <mholla8@gmail.com>
2023-09-23 13:52:54 -0400

    feat(home): navbar and landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,19 +6,13 @@
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
 <AppShell class="max-w-screen-xl m-auto">
 	<svelte:fragment slot="header">
-		<AppBar background="">
-			<svelte:fragment slot="lead">
-				<a href="/" class="text-2xl font-semibold">Engage.gg</a>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a href="" target="_blank" class="px-1"> Login </a>
-			</svelte:fragment>
-		</AppBar>
+	<NavBar />
 	</svelte:fragment>
 	<slot />
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,14 @@
 <script setup lang="ts">
-	import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi';
-
-	import { mainnet, arbitrum } from '@wagmi/core/chains';
-	import { goto } from '$app/navigation';
-
-	// 1. Define constants
-	const projectId = 'eec88af39c5c45e33ecfe3d19002da7d';
-
-	// 2. Create wagmiConfig
-	const chains = [mainnet, arbitrum];
-	const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' });
-
-	// 3. Create modal
-	const modal = createWeb3Modal({ wagmiConfig, projectId, chains });
-
-	const clientID = import.meta.env.VITE_TWITCH_CLIENT_ID;
-	const redirectURI = import.meta.env.VITE_TWITCH_REDIRECT_URI;
-	const responseType = 'token';
-	function login() {
-		goto(
-			'https://id.twitch.tv/oauth2/authorize?client_id=' +
-				clientID +
-				'&redirect_uri=' +
-				redirectURI +
-				'&response_type=' +
-				responseType
-		);
-	}
+	import { login, connected } from '$lib/utils/WalletConnect';
 </script>
 
 <div class="h-screen flex flex-col w-full text-center py-8 px-4 lg:py-16 lg:px-12">
 	<div class="m-auto items-center">
-		<div class="p-4 text-4xl font-bold md:text-5xl lg:text-6xl">Reward your loyal fanbase</div>
+		<div class="p-4 text-4xl font-bold md:text-5xl lg:text-6xl">
+			Supercharge your fan interactions
+		</div>
 		<div class="text-xl mb-8 font-semibold lg:text-2xl sm:px-16 xl:px-48">
-			Engage helps you grow your fanbase by rewarding them for watching your games.
+			Reward your loyal fanbase with exclusive NFTs and experiences
 		</div>
 		<div
 			class="flex flex-col mb-8 lg:mb-32 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"
@@ -43,7 +18,7 @@
 				class="btn py-3 px-7 rounded-xl variant-filled-primary"
 				on:click={login}
 			>
-				Login with Twitch
+				Get Started
 			</button>
 			<!-- <button type="button" class="btn py-3 px-5 rounded-xl variant-ringed-secondary">
 				Learn more
