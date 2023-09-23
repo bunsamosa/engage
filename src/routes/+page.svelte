cb05db94158e28278249c0d449d6b76f168e3819
madhukar01 <mholla8@gmail.com>
2023-09-23 01:57:03 -0400

    feat(home): add twitch login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,32 @@
 <script setup lang="ts">
+	import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi';
+
+	import { mainnet, arbitrum } from '@wagmi/core/chains';
+	import { goto } from '$app/navigation';
+
+	// 1. Define constants
+	const projectId = 'eec88af39c5c45e33ecfe3d19002da7d';
+
+	// 2. Create wagmiConfig
+	const chains = [mainnet, arbitrum];
+	const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' });
+
+	// 3. Create modal
+	const modal = createWeb3Modal({ wagmiConfig, projectId, chains });
+
+	const clientID = import.meta.env.VITE_TWITCH_CLIENT_ID;
+	const redirectURI = import.meta.env.VITE_TWITCH_REDIRECT_URI;
+	const responseType = 'token';
+	function login() {
+		goto(
+			'https://id.twitch.tv/oauth2/authorize?client_id=' +
+				clientID +
+				'&redirect_uri=' +
+				redirectURI +
+				'&response_type=' +
+				responseType
+		);
+	}
 </script>
 
 <div class="h-screen flex flex-col w-full text-center py-8 px-4 lg:py-16 lg:px-12">
@@ -10,8 +38,12 @@
 		<div
 			class="flex flex-col mb-8 lg:mb-32 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"
 		>
-			<button type="button" class="btn py-3 px-7 rounded-xl variant-filled-primary">
-				Get Started
+			<button
+				type="button"
+				class="btn py-3 px-7 rounded-xl variant-filled-primary"
+				on:click={login}
+			>
+				Login with Twitch
 			</button>
 			<!-- <button type="button" class="btn py-3 px-5 rounded-xl variant-ringed-secondary">
 				Learn more
