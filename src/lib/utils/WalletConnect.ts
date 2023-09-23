628010d232c6466eb07ea033a58279438cf5bd3b
madhukar01 <mholla8@gmail.com>
2023-09-23 15:45:27 -0400

    fix(auth): store address to localstorage

--- src/lib/utils/WalletConnect.ts
+++ src/lib/utils/WalletConnect.ts
@@ -1,18 +1,20 @@
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi';
-import { mainnet, arbitrum } from '@wagmi/core/chains';
+import { polygon, polygonMumbai } from '@wagmi/core/chains';
 import { writable } from 'svelte/store';
 import { disconnect, watchAccount } from '@wagmi/core';
+import { goto } from '$app/navigation';
 
 
 // wallet connect setup and modal
 const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
-const chains = [mainnet, arbitrum];
+const chains = [polygon, polygonMumbai];
 
 const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' });
 const modal = createWeb3Modal({ wagmiConfig, projectId, chains });
 
 // data stores
-const address = writable('');
+const storedAddress = localStorage.getItem('address');
+const address = writable(storedAddress || '');
 const connected = writable(false);
 
 // open connection modal
@@ -25,10 +27,13 @@ watchAccount(async (account) => {
     if(account.isConnected) {
         connected.set(true);
         address.set(account.address);
+        localStorage.setItem('address', account.address);
         console.log('User is connected');
+        goto('/creator/onboard');
     } else {
         connected.set(false);
         address.set('');
+        localStorage.setItem('address', account.address);
         console.log('User is not connected');
     }
 });
