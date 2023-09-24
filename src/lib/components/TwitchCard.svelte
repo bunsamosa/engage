e532039c801380504e6dd16fcde9e83585a3922c
madhukar01 <mholla8@gmail.com>
2023-09-23 20:45:37 -0400

    feat(onboard): onboarding page

---
+++ src/lib/components/TwitchCard.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { authenticate, fetchProfile } from '$lib/utils/TwitchClient';
+	import { page } from '$app/stores';
+	import Spinner from '$lib/components/Spinner.svelte';
+    import { Avatar } from '@skeletonlabs/skeleton';
+
+
+	let twitchAuth = null;
+	let loading = false;
+	let profileData = null;
+
+	// handle redirection from twitch
+	const hashParams = new URLSearchParams($page.url.hash.slice(1));
+	if (hashParams.has('access_token')) {
+		loading = true;
+		twitchAuth = hashParams.get('access_token') || null;
+
+		if (twitchAuth !== null) {
+			// fetch user name
+			profileData = fetchProfile(twitchAuth);
+		}
+	}
+</script>
+
+<div class="card w-72 h-72 m-4 rounded-xl shadow-lg overflow-hidden
+        variant-filled flex flex-col items-center p-2"
+    >
+    <Avatar src="/images/twitch_logo.png" width="w-32" rounded="rounded-full" background="none" />
+    <div class="p-3 text-2xl">
+        Twitch
+    </div>
+    {#if !loading && profileData === null}
+        <button class="btn variant-filled-primary m-2" on:click={authenticate}>Connect</button>
+    {:else if profileData != null}
+        <h3 class="p-4">Connected to {localStorage.getItem('twitch_display_name')}!</h3>
+    {:else}
+        <Spinner />
+    {/if}
+</div>
---
+++ src/routes/creator/onboard/+page.svelte
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TwitchCard from "$lib/components/TwitchCard.svelte";
+</script>
+<div class="flex flex-col h-full">
+    <div class="text-center">
+        <div class="p-4 text-4xl font-bold md:text-3xl lg:text-4xl">Welcome to Engage!</div>
+        <div class="text-xl mb-8 font-semibold lg:text-2xl sm:px-16 xl:px-48">
+            Please connect your social accounts to get started.
+        </div>
+    </div>
+
+    <div class="flex flex-row flex-wrap overflow-y-scroll justify-center">
+        <TwitchCard/>
+    </div>
+</div>
